96043332ab73c06025ed10d106d30b2f33b24586
hxddh <hxddh@users.noreply.github.com>
2026-08-29 14:33:39 +0800

    refactor(agent): delete message-era task content adapter

--- frontend/src/components/TaskContent.tsx
+++
@@ -1,6 +0,0 @@
-/** Public Agent-task content exports. */
-export { AgentTaskResult as MessageCard, AgentTaskResult } from "./AgentTaskResult";
-export { ProposalCard } from "./AgentDecisionCard";
-export { ThinkingBubble, GroundingCard, FindingsCard, TriageCard } from "./AgentRuntimeArtifacts";
-export { AgentResultRenderer } from "./AgentResultRenderer";
-export { S3ErrorArtifact } from "./S3ErrorArtifact";
